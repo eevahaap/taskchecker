e631b346c022b54025e6d7c0d65f761e3262b7ea
eevahaap <eeva.haapakangas@helsinki.fi>
2015-04-12 18:44:23 +0300

    homepaage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root'students#index'
+  resources :homepages
+
+  root'homepages#index'
 
   resources :schoolclasses
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410195223) do
+ActiveRecord::Schema.define(version: 20150412153930) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "username"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20150410195223) do
     t.datetime "updated_at",  null: false
     t.integer  "question_id"
     t.integer  "student_id"
+  end
+
+  create_table "homepages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
