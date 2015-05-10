2a5d1be0a6440035025339fc0679a1d54d749efc
eevahaap <eeva.haapakangas@helsinki.fi>
2015-05-10 21:56:35 +0300

    testit

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
 
   it "has the username set correctly" do
-    student = Student.new username:"Pekka"
+    student = Student.new username:"Pasi"
 
-    student.username.should == "Pekka"
+    student.username.should == "Pasi"
   end
 
   it "is not saved without a password" do
-    student = Student.create username:"Pekka"
+    student = Student.create username:"Pasi"
 
     expect(student.valid?).to be(false)
     expect(Student.count).to eq(0)
@@ -18,26 +18,26 @@ RSpec.describe Student, type: :model do
   describe "is not saved" do
 
     it "with password that's too short" do
-      student = Student.create username: "Pekka", password: "A1"
+      student = Student.create username: "Pasi", password: "A1"
       expect(student.valid?).to be(false)
       expect(Student.count).to eq(0)
     end
 
     it "with password that doesn't contain a non-letter character" do
-      student = Student.create username: "Pekka", password: "abcdefgh"
+      student = Student.create username: "Pasi", password: "abcdefgh"
       expect(student.valid?).to be(false)
       expect(Student.count).to eq(0)
     end
 
     it "without a proper password" do
-      student = Student.create username: "Pekka"
+      student = Student.create username: "Pasi"
       expect(student.valid?).to be(false)
       expect(Student.count).to eq(0)
     end
   end
 
   describe "with a proper password" do
-    let(:student){ Student.create username:"Pekka", password:"Secret1" }
+    let(:student){ Student.create username:"Pasi", password:"Kissa1" }
 
     it "is saved" do
       expect(student).to be_valid
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Teacher, type: :model do
 
   it "has the username set correctly" do
-    teacher = Teacher.new username:"Pekka"
+    teacher = Teacher.new username:"Pasi"
 
-    teacher.username.should == "Pekka"
+    teacher.username.should == "Pasi"
   end
 
   it "is not saved without a password" do
-    teacher = Teacher.create username:"Pekka"
+    teacher = Teacher.create username:"Pasi"
 
     expect(teacher.valid?).to be(false)
     expect(Teacher.count).to eq(0)
@@ -18,26 +18,26 @@ RSpec.describe Teacher, type: :model do
   describe "is not saved" do
 
     it "with password that's too short" do
-      teacher = Teacher.create username: "Pekka", password: "A1"
+      teacher = Teacher.create username: "Pasi", password: "A1"
       expect(teacher.valid?).to be(false)
       expect(Teacher.count).to eq(0)
     end
 
     it "with password that doesn't contain a non-letter character" do
-      teacher = Teacher.create username: "Pekka", password: "abcdefgh"
+      teacher = Teacher.create username: "Pasi", password: "abcdefgh"
       expect(teacher.valid?).to be(false)
       expect(Teacher.count).to eq(0)
     end
 
     it "without a proper password" do
-      teacher = Teacher.create username: "Pekka"
+      teacher = Teacher.create username: "Pasi"
       expect(teacher.valid?).to be(false)
       expect(Teacher.count).to eq(0)
     end
   end
 
   describe "with a proper password" do
-    let(:teacher){ Teacher.create username:"Pekka", password:"Secret1" }
+    let(:teacher){ Teacher.create username:"Pasi", password:"Kissa1" }
 
     it "is saved" do
       expect(teacher).to be_valid
